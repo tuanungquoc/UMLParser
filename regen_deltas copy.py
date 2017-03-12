992a2ef51c6c65188b291da7ceea265737cdc926
Carlos Martinez <Carlos@Carloss-MacBook-Pro-3.local>
2017-03-11 23:37:01 -0800

    Add method to load data into directory

--- regen_deltas copy.py
+++ regen_deltas copy.py
@@ -59,4 +59,27 @@ def parse_uml(self,source,output):
     # LOGGER Instantiation:  This has to be done in every module to allow logging
     logger = logging.getLogger(__name__)
     logger.info("Received source and output:"+source + output)
+
+
     pass
+
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+if not isfile('cifar-10-python.tar.gz'):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='CIFAR-10 Dataset') as pbar:
+        urlretrieve(
+            'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+            'cifar-10-python.tar.gz',
+            pbar.hook)
+
+if not isdir(cifar10_dataset_folder_path):
+    with tarfile.open('cifar-10-python.tar.gz') as tar:
+        tar.extractall()
+        tar.close()
